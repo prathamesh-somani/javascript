aad04d09e7b57d003ab3086e5c6c64f223824025
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-05 19:47:06 +0000

    Date

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date()
+console.log (myDate);
+console.log (myDate.toString());
+console.log (myDate.toDateString());
+console.log (myDate.toLocaleString('en-IN'));
+console.log (myDate.toISOString());
+console.log (myDate.toJSON());
+console.log (myDate.toUTCString());
+
+console.log (typeof myDate);
