4a80fc44abf206043b83ddfce43d578a53f7ca5f
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-11 20:08:16 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while 
+
+let index = 0 // Variable defined
+// while (index<=10) {      // Condition
+//     // console.log (`Value of index is ${index}`);
+//     index = index+2   //Increment
+// }
+
+let array= ["flash","batman","superman"]
+
+let arr = 0
+while (arr<array.length){
+    // console.log (array[arr]);
+    arr++;
+}
+
+// do-while
+// First work is done then condition is checked
+let score = 1
+
+do {
+    console.log (`Score is ${score}`);
+    score++
+} while (score <=10);
