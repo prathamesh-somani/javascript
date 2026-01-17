57224352ab0d0c1b5f9b56cbe607c920a706d171
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-17 21:41:08 +0000

    event basics done

--- 08_events/eventbasics.js
+++ 08_events/eventbasics.js
@@ -1,1 +1,7 @@
 console.log("Getting started with event");
+
+// Javascript ke andar jitna bhi code hai , events hai woh sequentially run hote hai except few exceptions
+
+// Javascript is a sequentially run language
+
+// Browser events is an exception like async functions
